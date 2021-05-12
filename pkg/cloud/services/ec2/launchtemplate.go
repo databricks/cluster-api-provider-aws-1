dropbox-code/repo-9

LaunchTemplate: Return userdata hash from GetLaunchTemplate
The userdata hash will be used to check whether the actual userdata (in
the LaunchTemplate) matches the desired userdata (in a cluster object,
e.g. KubeadmConfig)

pkg/cloud/services/ec2/launchtemplate.go
@@ -30,13 +30,14 @@ import (
 	expinfrav1 "sigs.k8s.io/cluster-api-provider-aws/exp/api/v1alpha4"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/awserrors"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/scope"
+	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/services/userdata"
 )
 
 // GetLaunchTemplate returns the existing LaunchTemplate or nothing if it doesn't exist.
 // For now by name until we need the input to be something different
-func (s *Service) GetLaunchTemplate(id string) (*expinfrav1.AWSLaunchTemplate, error) {
+func (s *Service) GetLaunchTemplate(id string) (*expinfrav1.AWSLaunchTemplate, string, error) {
 	if id == "" {
-		return nil, nil
+		return nil, "", nil
 	}
 
 	s.scope.V(2).Info("Looking for existing LaunchTemplates")
@@ -49,13 +50,13 @@ func (s *Service) GetLaunchTemplate(id string) (*expinfrav1.AWSLaunchTemplate, e
 	out, err := s.EC2Client.DescribeLaunchTemplateVersions(input)
 	switch {
 	case awserrors.IsNotFound(err):
-		return nil, nil
+		return nil, "", nil
 	case err != nil:
 		s.scope.Info("", "aerr", err.Error())
 	}
 
 	if len(out.LaunchTemplateVersions) == 0 {
-		return nil, nil
+		return nil, "", nil
 	}
 
 	return s.SDKToLaunchTemplate(out.LaunchTemplateVersions[0])
@@ -217,7 +218,7 @@ func (s *Service) DeleteLaunchTemplate(id string) error {
 }
 
 // SDKToLaunchTemplate converts an AWS EC2 SDK instance to the CAPA instance type.
-func (s *Service) SDKToLaunchTemplate(d *ec2.LaunchTemplateVersion) (*expinfrav1.AWSLaunchTemplate, error) {
+func (s *Service) SDKToLaunchTemplate(d *ec2.LaunchTemplateVersion) (*expinfrav1.AWSLaunchTemplate, string, error) {
 	v := d.LaunchTemplateData
 	i := &expinfrav1.AWSLaunchTemplate{
 		Name: aws.StringValue(d.LaunchTemplateName),
@@ -245,7 +246,15 @@ func (s *Service) SDKToLaunchTemplate(d *ec2.LaunchTemplateVersion) (*expinfrav1
 		i.AdditionalSecurityGroups = append(i.AdditionalSecurityGroups, infrav1.AWSResourceReference{ID: id})
 	}
 
-	return i, nil
+	if v.UserData == nil {
+		return i, userdata.ComputeHash(nil), nil
+	}
+	decodedUserData, err := base64.StdEncoding.DecodeString(*v.UserData)
+	if err != nil {
+		return nil, "", errors.Wrap(err, "unable to decode UserData")
+	}
+
+	return i, userdata.ComputeHash(decodedUserData), nil
 }
 
 // LaunchTemplateNeedsUpdate checks if a new launch template version is needed

pkg/cloud/services/mock_services/ec2_machine_interface_mock.go
@@ -184,12 +184,13 @@ func (mr *MockEC2MachineInterfaceMockRecorder) GetInstanceSecurityGroups(arg0 in
 }
 
 // GetLaunchTemplate mocks base method
-func (m *MockEC2MachineInterface) GetLaunchTemplate(arg0 string) (*v1alpha40.AWSLaunchTemplate, error) {
+func (m *MockEC2MachineInterface) GetLaunchTemplate(arg0 string) (*v1alpha40.AWSLaunchTemplate, string, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetLaunchTemplate", arg0)
 	ret0, _ := ret[0].(*v1alpha40.AWSLaunchTemplate)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
+	ret1, _ := ret[1].(string)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
 }
 
 // GetLaunchTemplate indicates an expected call of GetLaunchTemplate

databricks/cluster-api-provider-aws-1

pkg/cloud/services/ec2/launchtemplate.go
@@ -30,13 +30,14 @@ import (
 	expinfrav1 "sigs.k8s.io/cluster-api-provider-aws/exp/api/v1alpha3"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/awserrors"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/scope"
+	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/services/userdata"
 )
 
 // GetLaunchTemplate returns the existing LaunchTemplate or nothing if it doesn't exist.
 // For now by name until we need the input to be something different
-func (s *Service) GetLaunchTemplate(launchTemplateName string) (*expinfrav1.AWSLaunchTemplate, error) {
+func (s *Service) GetLaunchTemplate(launchTemplateName string) (*expinfrav1.AWSLaunchTemplate, string, error) {
 	if launchTemplateName == "" {
-		return nil, nil
+		return nil, "", nil
 	}
 
 	s.scope.V(2).Info("Looking for existing LaunchTemplates")
@@ -49,14 +50,14 @@ func (s *Service) GetLaunchTemplate(launchTemplateName string) (*expinfrav1.AWSL
 	out, err := s.EC2Client.DescribeLaunchTemplateVersions(input)
 	switch {
 	case awserrors.IsNotFound(err):
-		return nil, nil
+		return nil, "", nil
 	case err != nil:
 		s.scope.Info("", "aerr", err.Error())
-		return nil, err
+		return nil, "", err
 	}
 
 	if len(out.LaunchTemplateVersions) == 0 {
-		return nil, nil
+		return nil, "", nil
 	}
 
 	return s.SDKToLaunchTemplate(out.LaunchTemplateVersions[0])
@@ -244,7 +245,7 @@ func (s *Service) DeleteLaunchTemplate(id string) error {
 }
 
 // SDKToLaunchTemplate converts an AWS EC2 SDK instance to the CAPA instance type.
-func (s *Service) SDKToLaunchTemplate(d *ec2.LaunchTemplateVersion) (*expinfrav1.AWSLaunchTemplate, error) {
+func (s *Service) SDKToLaunchTemplate(d *ec2.LaunchTemplateVersion) (*expinfrav1.AWSLaunchTemplate, string, error) {
 	v := d.LaunchTemplateData
 	i := &expinfrav1.AWSLaunchTemplate{
 		Name: aws.StringValue(d.LaunchTemplateName),
@@ -272,7 +273,15 @@ func (s *Service) SDKToLaunchTemplate(d *ec2.LaunchTemplateVersion) (*expinfrav1
 		i.AdditionalSecurityGroups = append(i.AdditionalSecurityGroups, infrav1.AWSResourceReference{ID: id})
 	}
 
-	return i, nil
+	if v.UserData == nil {
+		return i, userdata.ComputeHash(nil), nil
+	}
+	decodedUserData, err := base64.StdEncoding.DecodeString(*v.UserData)
+	if err != nil {
+		return nil, "", errors.Wrap(err, "unable to decode UserData")
+	}
+
+	return i, userdata.ComputeHash(decodedUserData), nil
 }
 
 // LaunchTemplateNeedsUpdate checks if a new launch template version is needed

pkg/cloud/services/mock_services/ec2_machine_interface_mock.go
@@ -184,12 +184,13 @@ func (mr *MockEC2MachineInterfaceMockRecorder) GetInstanceSecurityGroups(arg0 in
 }
 
 // GetLaunchTemplate mocks base method
-func (m *MockEC2MachineInterface) GetLaunchTemplate(arg0 string) (*v1alpha30.AWSLaunchTemplate, error) {
+func (m *MockEC2MachineInterface) GetLaunchTemplate(arg0 string) (*v1alpha30.AWSLaunchTemplate, string, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetLaunchTemplate", arg0)
 	ret0, _ := ret[0].(*v1alpha30.AWSLaunchTemplate)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
+	ret1, _ := ret[1].(string)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
 }
 
 // GetLaunchTemplate indicates an expected call of GetLaunchTemplate